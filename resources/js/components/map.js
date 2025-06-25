2731604151497a0ea0c54a1297ff530ff4123835
Bruno Silva <bsilva0x87@gmail.com>
2025-06-25 10:35:44 -0300

    feat: use light color map

--- resources/js/components/map.js
+++ resources/js/components/map.js
@@ -44,10 +44,11 @@ class MapComponent {
             position: 'bottomright'
         }).addTo(this.map);
         
-        // Add OpenStreetMap tiles
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            maxZoom: 19
+        // Add grayscale tiles using CartoDB Positron
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, © <a href="https://carto.com/attributions">CARTO</a>',
+            maxZoom: 19,
+            subdomains: 'abcd'
         }).addTo(this.map);
         
         // Create marker layer group
