422058329f113e192db277d53a98b1b84d7845d4
Bruno Silva <bsilva0x87@gmail.com>
2025-06-29 15:44:48 -0300

    feat: open graph and favicon resources

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,22 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Open Graph -->
+        <meta property="og:title" content="{{ $title ?? 'Programa Bira Carvalho (Maré)' }}">
+        <meta property="og:description" content="O Programa Bira Carvalho é um projeto de inclusão social que visa oferecer acesso aos espaços públicos para pessoas com deficiência. Ele é um esforço colaborativo entre a Prefeitura do Rio de Janeiro e a Bira Carvalho, uma empresa de tecnologia que desenvolveu uma plataforma de mapas interativos para facilitar a localização de espaços acessíveis.">
+        <meta property="og:image" content="{{ asset('assets/images/og-image.jpg') }}">
+        <meta property="og:url" content="{{ url()->current() }}">
+        <meta property="og:type" content="website">
+        <meta property="og:locale" content="pt-BR">
+
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicon/favicon-16x16.png') }}">
+        <link rel="icon" href="{{ asset('assets/favicon/favicon.ico') }}">
+        <link rel="manifest" href="{{ asset('assets/favicon/site.webmanifest') }}">
+        <meta name="theme-color" content="#653089">
         
         <!-- Leaflet CSS -->
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
